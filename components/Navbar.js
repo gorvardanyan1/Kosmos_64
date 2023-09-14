0da5ce2bb8500c9922c337e609981a930b9e8111
gorvardanyan1 <gorv887@gmail.com>
2023-09-14 18:15:04 +0400

    responsive navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import styles from "@/styles/Navbar.module.css"
+import { useState } from 'react';
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
     return (
         <header className={styles.header}>
-            <h1> <span>Ko</span>sm<span>os</span> 64</h1>
-            <nav className={styles.nav}>
+            {/* <h1> <span>Ko</span>sm<span>os</span> 64</h1> */}
+            {/* <nav className={styles.nav}>
                 <ul>
                     <li>
                         <a href="#home">Home</a>
@@ -23,6 +30,34 @@ const Navbar = () => {
                         <img src="taxi.png" alt="taxiImg" />
                     </li>
                 </ul>
+            </nav> */}
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <h1 className='logo'> <span>Ko</span>sm<span>os</span> 64</h1>
+                    <div className={`menu-button ${isOpen ? 'open' : ''}`} onClick={toggleNavbar}>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                    </div>
+                    <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+                        <li>
+                            <a href="#">Home</a>
+                            <img src="taxi.png" alt="taxiImg" />
+                        </li>
+                        <li>
+                            <a href="#">About</a>
+                            <img src="taxi.png" alt="taxiImg" />
+                        </li>
+                        <li>
+                            <a href="#">Services</a>
+                            <img src="taxi.png" alt="taxiImg" />
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                            <img src="taxi.png" alt="taxiImg" />
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </header>
     )
